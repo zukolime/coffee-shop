597a7e9350729183a2fbdeb439f32845260c9f80
zukolime <alena.perminova.1993@gmail.com>
2025-04-10 12:38:27 +0500

    NavBarComponent, FooterComponent,CardComponent are created

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div id="app">Hello Vue.JS</div>
+  <div class="wrapper">
+    <hero-view />
+    <footer-component />
+  </div>
 </template>
 
 <script>
-export default {};
+import HeroView from "@/views/HeroView.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: { HeroView, FooterComponent },
+};
 </script>
 
 <style></style>
